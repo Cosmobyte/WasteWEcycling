74ec7e753e9b3d3dd419c506f642efae7e7e2bab
Cosmobyte <cosmin.mirel18@gmail.com>
2022-01-05 14:07:24 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from fastbook import load_learner
+from fastai.vision.all import *
+from fastai.data.external import *
+
+import pickle
+
+
+
+model = load_learner("my_export1.pkl")
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+def process_image():
+    file = request.files['file']
+    # Read the image via file.stream
+    
+    img_pil = PILImage.create(file)
+  
+    pred,pred_idx,probs = model.predict(img_pil)
+
+    return jsonify({'prediction':str(pred),'probability':str(probs[pred_idx])})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
