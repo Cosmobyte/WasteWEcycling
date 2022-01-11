d52e5b47181c5df695647a858d19a1a17ecf807d
Cosmobyte <cosmin.mirel18@gmail.com>
2022-01-11 03:52:50 +0100

    changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
-from fastbook import load_learner
 from fastai.vision.all import *
 from fastai.data.external import *
+# import urllib.request
 
-import pickle
+# MODEL_URL = "https://drive.google.com/uc?export=download&id=1kJJTB9mhzTS_xrui1eFSct96KeRNo9BY"
+# urllib.request.urlretrieve(MODEL_URL, "my_export1.pkl")
 
-
+# # model = load_learner("my_export1.pkl")
+# model = load_learner(Path("."), "my_export1.pkl")
 
 model = load_learner("my_export1.pkl")
 
@@ -13,15 +15,14 @@ app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
 def process_image():
-    file = request.files['image0']
-    # Read the image via file.stream
     
-    img_pil = PILImage.create(file)
+    imagefile = request.files['image0']
+    img_pil = PILImage.create(imagefile)
   
     pred,pred_idx,probs = model.predict(img_pil)
 
     return jsonify({'prediction':str(pred),'probability':str(probs[pred_idx])})
-
+   
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000), debug=True)
